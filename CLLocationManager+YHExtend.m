03566d2e669c0edc371a5a3540963b6df62446b5
Survivorsfyh <survivorsfyh@163.com>
2020-01-15 15:33:44 +0800

    方法优化
    实现类方法优化

--- CLLocationManager+YHExtend.m
+++ CLLocationManager+YHExtend.m
@@ -10,25 +10,48 @@
 #import <objc/runtime.h>
 
 @implementation CLLocationManager (YHExtend)
+    
+static inline void pg_swizzleSelector(Class theClass, SEL originalSelector, SEL swizzledSelector) {
+    Method originaMethod = class_getInstanceMethod(theClass, originalSelector);
+    Method swizzledMethod = class_getInstanceMethod(theClass, swizzledSelector);
+    method_exchangeImplementations(originaMethod, swizzledMethod);
+}
 
 + (void)load {
-    if (9.0 <= [UIDevice currentDevice].systemVersion.floatValue) {
-        method_exchangeImplementations(class_getInstanceMethod(self.class, NSSelectorFromString(@"swizzledSetAllowsBackgroundLocationUpdates:")),
-                class_getInstanceMethod(self.class, @selector(swizzledSetAllowsBackgroundLocationUpdates:)));
+    if (9 <= [UIDevice currentDevice].systemVersion.floatValue) {
+        pg_swizzleSelector(UIDevice.class,
+                           @selector(endGeneratingDeviceOrientationNotifications),
+                           @selector(pgEndGeneratingDeviceOrientationNotifications));
     }
+    
+//     if (9.0 <= [UIDevice currentDevice].systemVersion.floatValue) {
+//         method_exchangeImplementations(class_getInstanceMethod(self.class, NSSelectorFromString(@"swizzledSetAllowsBackgroundLocationUpdates:")),
+//                 class_getInstanceMethod(self.class, @selector(swizzledSetAllowsBackgroundLocationUpdates:)));
+//     }
 }
 
-- (void)swizzledSetAllowsBackgroundLocationUpdates:(BOOL)allow {
-    if (allow) {
-        NSArray *backgroundModes = [[NSBundle mainBundle].infoDictionary objectForKey:@"UIBackgroundModes"];
-        if (backgroundModes && [backgroundModes containsObject:@"location"]) {
-            [self swizzledSetAllowsBackgroundLocationUpdates:allow];
-        } else {
-            NSLog(@"App 想设置后台定位，但 App 的 info.plist 里并没有后台定位权限");
-        }
-    } else {
-        [self swizzledSetAllowsBackgroundLocationUpdates:allow];
+- (void)pgEndGeneratingDeviceOrientationNotifications {
+    NSLog(@"swizzledSetAllowsBackgroundLocationUpdates isMainThread:%d", [NSThread isMainThread]);
+    if (![NSThread isMainThread]) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self pgEndGeneratingDeviceOrientationNotifications];
+        });
+        return;
     }
+    [self pgEndGeneratingDeviceOrientationNotifications];
 }
+
+// - (void)swizzledSetAllowsBackgroundLocationUpdates:(BOOL)allow {
+//     if (allow) {
+//         NSArray *backgroundModes = [[NSBundle mainBundle].infoDictionary objectForKey:@"UIBackgroundModes"];
+//         if (backgroundModes && [backgroundModes containsObject:@"location"]) {
+//             [self swizzledSetAllowsBackgroundLocationUpdates:allow];
+//         } else {
+//             NSLog(@"App 想设置后台定位，但 App 的 info.plist 里并没有后台定位权限");
+//         }
+//     } else {
+//         [self swizzledSetAllowsBackgroundLocationUpdates:allow];
+//     }
+// }
 
 @end
